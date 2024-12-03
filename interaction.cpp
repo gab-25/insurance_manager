c57cf5b90c3c5f62bd2678a82142a5fbf7fd9536
Gab25 <gabrielesorci.25@gmail.com>
2024-12-03 19:13:48 +0100

    fix interaction date

--- interaction.cpp
+++ interaction.cpp
@@ -4,19 +4,19 @@ class Interaction
 {
 private:
     std::string type;
-    std::string data;
+    std::string date;
     std::string description;
 
 public:
-    Interaction(std::string _tipo, std::string _data, std::string _descrizione)
-        : type(_tipo), data(_data), description(_descrizione) {}
+    Interaction(std::string _tipo, std::string _date, std::string _descrizione)
+        : type(_tipo), date(_date), description(_descrizione) {}
 
     std::string getTipo() const { return type; }
-    std::string getData() const { return data; }
+    std::string getDate() const { return date; }
     std::string getDescrizione() const { return description; }
 
     std::string toString() const
     {
-        return "Tipo: " + type + ", Data: " + data + ", Descrizione: " + description;
+        return "Tipo: " + type + ", Data: " + date + ", Descrizione: " + description;
     }
 };
